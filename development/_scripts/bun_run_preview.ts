2b2525959f4ad4c05037d9ccfe8baa4af81b7078
Gerold Penz <gerold-penz@users.noreply.github.com>
2025-01-09 18:36:34 +0100

    Update bun_run_preview.ts

--- development/_scripts/bun_run_preview.ts
+++ development/_scripts/bun_run_preview.ts
@@ -4,8 +4,8 @@ import { join } from "node:path"
 
 
 $.cwd(join(__dirname, "..", "sveltekit_src"))
-$.env({FORCE_COLOR: "1"})
 
+let exitCode = 1
 try {
     exitCode = (await $`bun run preview`).exitCode
 } finally {
