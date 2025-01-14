ed126a4fd7f55f01f8db9e9301bbb48ebff84dae
Gerold Penz <gerold@halvar.at>
2025-01-14 16:43:11 +0100

    Back to Bootstrap again.

---
+++ development/sveltekit_src/src/routes/+page.server.ts
@@ -0,0 +1,11 @@
+import { redirect, type ServerLoad } from "@sveltejs/kit"
+import type { Session } from "@auth/core/types"
+import { status } from "http-status"
+
+
+export const load: ServerLoad = async ({locals, url}) => {
+    const session: Session | undefined = locals?.session || undefined
+    if (session) {
+        return redirect(status.FOUND, "/contacts")
+    }
+}
---
+++ development/sveltekit_src/src/routes/contacts/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+</script>
+
+
+<!-- Page head BEGIN -->
+<svelte:head>
+  <title>Contacts Organizer - Contacts</title>
+</svelte:head>
+<!-- Page head END -->
+
+
+<!-- Main content BEGIN -->
+<main class="container-fluid py-2">
+
+  <div class="row g-2">
+
+    <div class="col-12 col-md-5 col-lg-4 col-xl-3">
+      <div class="card text-bg-light h-100">
+        <div class="card-body">
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+          [Sidebar]<br>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-7 col-lg-8 col-xl-9">
+      <div class="card h-100">
+        <div class="card-body">
+          [Content]<br>
+          [Content]<br>
+          [Content]<br>
+          [Content]<br>
+          [Content]<br>
+          [Content]<br>
+          [Content]<br>
+          [Content]<br>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+</main>
+<!-- Main content END -->
+
