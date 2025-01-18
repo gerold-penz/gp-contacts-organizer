a59d8ca5f2dae1bb5be6b43acff92c33071f3ca8
Gerold Penz <gerold@halvar.at>
2025-01-18 16:08:55 +0100

    Breadcrumbs

--- development/sveltekit_src/src/lib/server/vcardsParsed/index.ts
+++ development/sveltekit_src/src/lib/server/vcardsParsed/index.ts
@@ -37,8 +37,128 @@ export namespace VcardsParsed {
         const cards = parseVCards(vcardData, keepDefective)
         if (cards?.vCards?.length) {
             set(username, addressBookUrlHash, vcardUrlHash, cards.vCards[0])
-
         }
+
+
+        // BEGIN:VCARD
+        // VERSION:3.0
+        // PRODID:-//Sabre//Sabre VObject 4.5.4//EN
+        // UID:c07bc863-dd67-47e0-a803-8a7d235e4765
+        // FN:Agnes Rimml
+        // N:Rimml;Agnes;;;
+        // TEL;TYPE=cell:+436504091122
+        // ADR;TYPE=home;LABEL=Telfs:;;;Telfs;;;
+        // ITEM1.X-ABDATE;VALUE=DATE-AND-OR-TIME:20250117
+        // ITEM1.X-ABLABEL:_$!<Anniversary>!$_
+        // X-ANNIVERSARY;VALUE=DATE:20250117
+        // REV;VALUE=DATE-AND-OR-TIME:20250117T211228Z
+        // END:VCARD
+
+        // {
+        //   "nags": [
+        //     {
+        //       "key": "VALUE_INVALID",
+        //       "description": "Invalid property value",
+        //       "isError": true,
+        //       "attributes": {
+        //         "property": "VERSION",
+        //         "line": "VERSION:3.0"
+        //       }
+        //     }
+        //   ],
+        //   "BEGIN": {
+        //     "value": "VCARD"
+        //   },
+        //   "VERSION": {
+        //     "value": "3.0"
+        //   },
+        //   "PRODID": {
+        //     "value": "-//Sabre//Sabre VObject 4.5.4//EN"
+        //   },
+        //   "UID": {
+        //     "value": "c07bc863-dd67-47e0-a803-8a7d235e4765"
+        //   },
+        //   "FN": [
+        //     {
+        //       "value": "Agnes Rimml"
+        //     }
+        //   ],
+        //   "N": {
+        //     "value": {
+        //       "familyNames": [
+        //         "Rimml"
+        //       ],
+        //       "givenNames": [
+        //         "Agnes"
+        //       ],
+        //       "additionalNames": [
+        //         ""
+        //       ],
+        //       "honorificPrefixes": [
+        //         ""
+        //       ],
+        //       "honorificSuffixes": [
+        //         ""
+        //       ]
+        //     }
+        //   },
+        //   "TEL": [
+        //     {
+        //       "parameters": {
+        //         "TYPE": [
+        //           "CELL"
+        //         ]
+        //       },
+        //       "value": "+436504091122"
+        //     }
+        //   ],
+        //   "ADR": [
+        //     {
+        //       "parameters": {
+        //         "TYPE": [
+        //           "HOME"
+        //         ]
+        //       },
+        //       "value": {
+        //         "postOfficeBox": [
+        //           ""
+        //         ],
+        //         "extendedAddress": [
+        //           ""
+        //         ],
+        //         "streetAddress": [
+        //           ""
+        //         ],
+        //         "locality": [
+        //           "Telfs"
+        //         ],
+        //         "region": [
+        //           ""
+        //         ],
+        //         "postalCode": [
+        //           ""
+        //         ],
+        //         "countryName": [
+        //           ""
+        //         ]
+        //       }
+        //     }
+        //   ],
+        //   "REV": {
+        //     "parameters": {
+        //       "VALUE": "DATE-AND-OR-TIME"
+        //     },
+        //     "value": "20250117T214415Z"
+        //   },
+        //   "END": {
+        //     "value": "VCARD"
+        //   },
+        //   "hasErrors": true
+        // }
+
+
+
+
     }
 
 
