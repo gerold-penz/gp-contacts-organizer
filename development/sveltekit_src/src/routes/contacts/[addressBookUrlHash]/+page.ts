2ffb1becffc8d2ba8835bc67277474d0fa3c8bcd
Gerold Penz <gerold@halvar.at>
2025-01-19 14:59:12 +0100

    Sorting and filtering prepared.

---
+++ development/sveltekit_src/src/routes/contacts/[addressBookUrlHash]/+page.ts
@@ -0,0 +1,4 @@
+// Only client side rendering because of sorting and filtering of the contacts.
+// The sort order and filter string is only in the browser available.
+export const csr = true
+export const ssr = false
--- development/sveltekit_src/src/routes/contacts/[addressBookUrlHash]/ContactsTable.svelte
+++ development/sveltekit_src/src/routes/contacts/[addressBookUrlHash]/ContactsTable.svelte
@@ -1,9 +1,69 @@
 <script lang="ts">
     import { page } from "$app/state"
     import type { VcardParsed } from "$lib/interfaces"
+    import { PersistedState } from "runed"
+    import { LOCAL_STORAGE_PREFIX } from "$lib/constants"
+    import { ArrowDownAz, ArrowDownZa } from "lucide-svelte"
 
 
-    const activeVcardsParsed = $derived<VcardParsed[]>(page.data.activeVcardsParsed)
+    const sortByPersisted = new PersistedState(
+        `${LOCAL_STORAGE_PREFIX}:sortBy`, "FN", {storage: "local"}
+    )
+
+    $inspect(sortByPersisted.current)
+
+    const sortBy = $derived(sortByPersisted.current)
+    const sortDirectionPersisted = new PersistedState<"asc" | "desc">(
+        `${LOCAL_STORAGE_PREFIX}:sortDirection`, "asc", {storage: "local"}
+    )
+
+    $inspect(sortDirectionPersisted.current)
+
+    const sortDirection = $derived(sortDirectionPersisted.current)
+    const activeVcards = $derived<VcardParsed[]>(page.data.activeVcardsParsed)
+    const fulltextFilter = $state<string>("")
+
+
+    // ToDo: Filter
+    const filteredVcards = $derived.by(() => {
+        if (!fulltextFilter) return activeVcards
+
+
+        // TEST
+        return activeVcards
+    })
+
+
+    // Sort
+    const sortedVcards = $derived.by(() => {
+        return filteredVcards.toSorted((a, b) => {
+
+            let aValue: string
+            let bValue: string
+
+            if (sortBy === "TEL") {
+                aValue = a.vcardParsed.TEL?.[0].value ?? ""
+                bValue = b.vcardParsed.TEL?.[0].value ?? ""
+            } else {
+                // FN is the default
+                aValue = a.vcardParsed.FN?.[0].value ?? ""
+                bValue = b.vcardParsed.FN?.[0].value ?? ""
+            }
+
+            if (sortDirection === "asc") {
+                if (aValue > bValue) return 1
+                if (aValue < bValue) return -1
+            } else {
+                if (aValue > bValue) return -1
+                if (aValue < bValue) return 1
+            }
+
+            // Default
+            return 0
+        })
+    })
+
+
 </script>
 
 
@@ -12,7 +72,39 @@
   <tr>
     <th><input class="form-check-input" type="checkbox"/></th>
     <th></th>
-    <th scope="row">Full Name</th>
+
+    <!-- Full name header BEGIN -->
+    <th scope="row">
+
+      <button
+        class="btn btn-link text-dark text-decoration-none fw-bold d-inline-flex align-items-center m-0 p-0 border-0"
+        onclick={() => {
+            sortByPersisted.current = "FN"
+            if (sortDirection === "asc"){
+                sortDirectionPersisted.current = "desc"
+            } else {
+                sortDirectionPersisted.current = "asc"
+            }
+        }}
+      >
+        <!-- Label BEGIN -->
+        <span>Full Name</span>
+        <!-- Label END -->
+
+        <!-- Sorting BEGIN -->
+        {#if sortBy === "FN"}
+          {#if sortDirection === "asc"}
+            <ArrowDownAz class="ms-2" size="20"/>
+          {:else}
+            <ArrowDownZa class="ms-2" size="20"/>
+          {/if}
+        {/if}
+        <!-- Sorting END -->
+      </button>
+
+    </th>
+    <!-- Full name header END -->
+
     <th scope="row">City</th>
     <th scope="row">Phone number</th>
     <th scope="row">Email</th>
@@ -21,7 +113,7 @@
   <tbody>
 
   <!-- Row BEGIN -->
-  {#each activeVcardsParsed as item}
+  {#each sortedVcards as vcard}
     <tr>
 
       <!-- Checkbox col BEGIN -->
@@ -54,30 +146,30 @@
 
       <!-- Full name col BEGIN -->
       <td>
-        {item.vcardParsed.FN[0].value}
+        {vcard.vcardParsed.FN[0].value}
       </td>
       <!-- Full name col END -->
 
       <!-- City col BEGIN -->
       <td>
-        {#if item.vcardParsed?.ADR?.length}
-          {item.vcardParsed.ADR[0].value?.locality?.[0]}
+        {#if vcard.vcardParsed?.ADR?.length}
+          {vcard.vcardParsed.ADR[0].value?.locality?.[0]}
         {/if}
       </td>
       <!-- City col END -->
 
       <!-- Phone col BEGIN -->
       <td class="text-nowrap text-truncate" style="max-widthx: 18em;">
-        {#if item.vcardParsed?.TEL?.length}
-          {item.vcardParsed.TEL[0].value}
+        {#if vcard.vcardParsed?.TEL?.length}
+          {vcard.vcardParsed.TEL[0].value}
         {/if}
       </td>
       <!-- Phone col END -->
 
       <!-- Email col BEGIN -->
       <td class="text-nowrap text-truncate" style="max-width: 18em;">
-        {#if item.vcardParsed?.EMAIL?.length}
-          {item.vcardParsed.EMAIL[0].value}
+        {#if vcard.vcardParsed?.EMAIL?.length}
+          {vcard.vcardParsed.EMAIL[0].value}
         {/if}
       </td>
       <!-- Email col END -->
@@ -168,3 +260,8 @@
 
   </tbody>
 </table>
+
+
+<style lang="scss">
+</style>
+
